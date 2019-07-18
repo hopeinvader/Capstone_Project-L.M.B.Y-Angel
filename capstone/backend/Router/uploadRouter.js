9b1d3d8a13cccac296b664c24f08fd458f4f6847
JihyupRyu <45484929+JihyupRyu@users.noreply.github.com>
2019-07-18 17:33:27 +0800

    Update uploadRouter.js

--- capstone/backend/Router/uploadRouter.js
+++ capstone/backend/Router/uploadRouter.js
@@ -27,12 +27,7 @@ const profileImgUpload = multer({
   checkFileType( file, cb );
  }
 }).single('profileImage');
-/**
- * Check File Type
- * @param file
- * @param cb
- * @return {*}
- */
+
 function checkFileType( file, cb ){
  const filetypes = /jpeg|jpg|png|gif/;
  const extname = filetypes.test( path.extname( file.originalname ).toLowerCase());
@@ -43,28 +38,17 @@ if( mimetype && extname ){
   cb( 'Error: Images Only!' );
  }
 }
-/**
- * @route POST api/profile/business-img-upload
- * @desc Upload post image
- * @access public
- */
+
 router.post( '/profile-img-upload', ( req, res ) => {
 profileImgUpload( req, res, ( error ) => {
-  // console.log( 'requestOkokok', req.file );
-  // console.log( 'error', error );
   if( error ){
-   console.log( 'errors', error );
    res.json( { error: error } );
   } else {
-   // If File not found
    if( req.file === undefined ){
-    console.log( 'Error: No File Selected!' );
     res.json( 'Error: No File Selected' );
    } else {
-    // If Success
     const imageName = req.file.key;
     const imageLocation = req.file.location;
-// Save the file name into database into profile model
 res.json( {
      image: imageName,
      location: imageLocation
@@ -74,54 +58,4 @@ res.json( {
  });
 });
 
-// Multiple File Uploads ( max 4 )
-// const uploadsBusinessGallery = multer({
-//  storage: multerS3({
-//   s3: s3,
-//   bucket: 'cpshpupload',
-//   acl: 'public-read',
-//   key: function (req, file, cb) {
-//    cb( null, path.basename( file.originalname, path.extname( file.originalname ) ) + '-' + Date.now() + path.extname( file.originalname ) )
-//   }
-//  }),
-//  limits:{ fileSize: 20000000 }, // In bytes: 2000000 bytes = 2 MB
-//  fileFilter: function( req, file, cb ){
-//   checkFileType( file, cb );
-//  }
-// }).array( 'galleryImage', 4 );
-// /**
-//  * @route POST /api/profile/business-gallery-upload
-//  * @desc Upload business Gallery images
-//  * @access public
-//  */
-// router.post('/multiple-file-upload', ( req, res ) => {
-// uploadsBusinessGallery( req, res, ( error ) => {
-//   console.log( 'files', req.files );
-//   if( error ){
-//    console.log( 'errors', error );
-//    res.json( { error: error } );
-//   } else {
-//    // If File not found
-//    if( req.files === undefined ){
-//     console.log( 'Error: No File Selected!' );
-//     res.json( 'Error: No File Selected' );
-//    } else {
-//     // If Success
-//     let fileArray = req.files,
-//      fileLocation;
-// const galleryImgLocationArray = [];
-//     for ( let i = 0; i < fileArray.length; i++ ) {
-//      fileLocation = fileArray[ i ].location;
-//      console.log( 'filenm', fileLocation );
-//      galleryImgLocationArray.push( fileLocation )
-//     }
-//     // Save the file name into database
-// res.json( {
-//      filesArray: fileArray,
-//      locationArray: galleryImgLocationArray
-//     } );
-//    }
-//   }
-//  });
-// });
 module.exports = router;
